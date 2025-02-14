52705b1f8d6494abff6a344c67a8562e064dc2a8
Viktor Kalyn <viktorkalynwebdev@gmail.com>
2025-02-13 22:29:06 -1000

    Automated the questions with the help of loops

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
 "use strict";
 const numberOfFilms = +prompt(`How many movies you already watched?`, "");
 
-const a = prompt(`One of the last films you watched?`, ""),
-  b = prompt(`How do you rate this film?`, ""),
-  c = prompt(`One of the last films you watched?`, ""),
-  d = prompt(`How do you rate this film?`, "");
-
 const personalMovieDB = {
   count: numberOfFilms,
   movies: {},
@@ -13,7 +8,25 @@ const personalMovieDB = {
   genres: [],
   privat: false,
 };
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+
+for (let i = 0; (i = 2); i++) {
+  const movie = prompt(`One of the last films you watched?`, ""),
+    rate = prompt(`How do you rate this film?`, "");
+  if (
+    movie != null &&
+    rate != null &&
+    movie != "" &&
+    rate != "" &&
+    movie.length < 50
+  ) {
+    personalMovieDB.movies[movie] = rate;
+    console.log("done");
+  } else {
+    alert(
+      "Your answer cannot be null or an empty string and must be shorter than 50 characters"
+    );
+    i--;
+  }
+}
 
 console.log(personalMovieDB);

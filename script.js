c7051c0cfa3f4da7a06520ec6cc6d85f071aac68
Viktor Kalyn <viktorkalynwebdev@gmail.com>
2025-02-14 17:50:37 -1000

    Refactoring the input of the question about your most recent movie viewing into a function

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
 "use strict";
 let numberOfFilms;
 
-const personalMovieDB = {
-  count: numberOfFilms,
-  movies: {},
-  actors: {},
-  genres: [],
-  privat: false,
-};
-
 function start() {
   numberOfFilms = prompt(`How many movies you already watched?`, "");
   while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
@@ -19,6 +11,14 @@ function start() {
 }
 start();
 
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
 function classifyMovieFan() {
   if (personalMovieDB.count > 0 && personalMovieDB.count < 10) {
     alert("Watched a fairly small number of movies");
@@ -26,34 +26,38 @@ function classifyMovieFan() {
     alert("You're a classic viewer");
   } else if (personalMovieDB.count > 20) {
     alert("You're a movie geek");
-  } else if (personalMovieDB.count === 0) {
+  } else if (personalMovieDB.count == 0) {
     alert(
       "You haven't watched any movies,  you can't use the movie library, come back after you've watched some movies"
     );
   }
 }
 classifyMovieFan();
-for (let i = 0; i < 2; i++) {
-  if (personalMovieDB.count === 0) {
-    break;
-  }
-  const movie = prompt(`One of the last films you watched?`, ""),
-    rate = prompt(`How do you rate this film?`, "");
-  if (
-    movie != null &&
-    rate != null &&
-    movie != "" &&
-    rate != "" &&
-    movie.length < 50
-  ) {
-    personalMovieDB.movies[movie] = rate;
-    console.log("done");
-  } else {
-    alert(
-      "Your answer cannot be null or an empty string and must be shorter than 50 characters"
-    );
-    i--;
+
+function addFilmToList() {
+  for (let i = 0; i < 2; i++) {
+    if (personalMovieDB.count === 0) {
+      break;
+    }
+    const movie = prompt(`One of the last films you watched?`, ""),
+      rate = prompt(`How do you rate this film?`, "");
+    if (
+      movie != null &&
+      rate != null &&
+      movie != "" &&
+      rate != "" &&
+      movie.length < 50
+    ) {
+      personalMovieDB.movies[movie] = rate;
+      console.log("done");
+    } else {
+      alert(
+        "Your answer cannot be null or an empty string and must be shorter than 50 characters"
+      );
+      i--;
+    }
   }
 }
 
+addFilmToList();5
 console.log(personalMovieDB);
